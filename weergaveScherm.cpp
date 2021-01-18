eb1e5207a2480a9c7ed8398f0357c0c76f20bc3c
Joris Goosen <Joris@JorisGoosen.nl>
2021-01-18 02:01:59 +0100

    lijkt beter

--- weergaveScherm.cpp
+++ weergaveScherm.cpp
@@ -228,9 +228,9 @@ void weergaveScherm::maakVolumeTextuur(const std::string & textuurNaam, glm::uve
 
 	glBindTexture(GL_TEXTURE_3D, texture);
 
-	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 	
 	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
