07f78699582ba054fa43d3d75af830a123576a7d
Joris Goosen <Joris@JorisGoosen.nl>
2021-02-16 21:32:14 +0100

    herhalen kan wel beter standaard zijn dan clampen

--- weergaveScherm.cpp
+++ weergaveScherm.cpp
@@ -230,8 +230,8 @@ void weergaveScherm::laadData(const std::string & textuurNaam, size_t breedte, s
 {
 	glBindTexture(GL_TEXTURE_2D, _texturen[textuurNaam]);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
