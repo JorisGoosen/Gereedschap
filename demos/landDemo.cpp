cb71c057c06cc0929c95cfd61bdafba2cfdc88e2
Joris Goosen <Joris@JorisGoosen.nl>
2023-08-01 19:16:21 +0200

    noice land met driehoekies

--- demos/landDemo.cpp
+++ demos/landDemo.cpp
@@ -5,24 +5,26 @@
 
 int main(int argc, const char * argv[])
 {
-	bool toonHetLand = argc > 1 && argv[1] == std::string("toonHetLand");
+	int 	vermindering 	= argc == 1 ? 0 : std::stoi(argv[1]),
+		 	deling 			= pow(2, vermindering);
 
-	weergaveSchermPerspectief scherm("Marcheer Demo", 1024, 1024, false);
+	weergaveSchermPerspectief scherm("Land Demo", 1024, 1024, false);
 	
 	scherm.maakShader("landDemo", 	"shaders/landDemo.vert", 	"shaders/landDemo.frag");
 	
 	glm::uvec2 landGrootte = scherm.laadTextuurUitPng("plaatjes/handLand.png", "handLand", false, false, true);
 
-	vierkantRooster landRooster(landGrootte.x, landGrootte.y);
+	vierkantRooster landRooster(landGrootte.x/deling, landGrootte.y/deling);
 	
 	float rot = 0.;
 	while(!scherm.stopGewenst())
 	{
 		scherm.herberekenProjectie();
-		scherm.zetModelZicht(glm::rotate(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -0.5f, -1.5f)), rot, glm::normalize(glm::vec3(0.0f, 1.0f, 0.0f))));
+		scherm.zetModelZicht(glm::rotate(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -0.25f, -1.0f)), rot, glm::normalize(glm::vec3(0.0f, 1.0f, 0.0f))));
 		scherm.bereidWeergevenVoor("landDemo");
 		scherm.bindTextuur("handLand", 0);
-		glUniform1i(glGetUniformLocation(scherm.huidigProgramma(), "handLand"), 0);
+		glUniform1i(glGetUniformLocation(scherm.huidigProgramma(), "handLand"), 	0);
+		glUniform1i(glGetUniformLocation(scherm.huidigProgramma(), "vermindering"), vermindering);
 		landRooster.tekenJezelf();
 		scherm.rondWeergevenAf();
 
--- shaders/landDemo.vert
+++ shaders/landDemo.vert
@@ -5,8 +5,10 @@ layout(location = 1) in highp vec2 vTex;
 
 uniform mat4 projectie;
 uniform mat4 modelView;
+uniform int vermindering;
 
 out highp vec2 textuur;
+out highp float hoogte;
 
 uniform sampler2D handLand;
 
@@ -15,7 +17,9 @@ void main()
 	textuur =  vTex;// (vTex + vec2(1.0)) / 2.0;
 
 	highp vec4 pos = vec4(vPos, 1.0);
-	pos.y = texture(handLand, textuur).r;
+	pos.y = textureLod(handLand, textuur, float(vermindering)).r;
 
 	gl_Position = projectie * modelView * pos;
+
+	hoogte = pos.y;
 }
